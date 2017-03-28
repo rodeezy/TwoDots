98907d13c28fd508f3d042410215e8f9fc530085
Ryan Odening <ryanodening@Ryans-MacBook-Pro.local>
2017-03-28 10:18:28 -0700

    add dots to board with colors

--- frontend/Board.js
+++ frontend/Board.js
@@ -1,9 +1,28 @@
-function init() {
-  var stage = new createjs.Stage("demoCanvas");
-  var circle = new createjs.Shape();
-  circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 10);
-  circle.x = 100;
-  circle.y = 100;
-  stage.addChild(circle);
-  stage.update();
+class Board {
+  constructor() {
+    this.canvas = document.getElementById('Canvas');
+    this.stage = new createjs.Stage("Canvas");
+    this.colors = ['#fecd6c', '#77c298', '#a4547d', '#e84d60', "DeepSkyBlue"]
+  }
+
+  randColor(){
+    return this.colors[Math.floor(Math.random() * this.colors.length)];
+  }
+
+  makeCircle(xPos, yPos) {
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill(this.randColor()).drawCircle(0, 0, 10);
+    circle.x = xPos;
+    circle.y = yPos;
+    this.stage.addChild(circle);
+    this.stage.update();
+  }
+
+  makeStage(){
+    for(let i=0;i<this.canvas.width;i+=10){
+      this.makeCircle(i, i);
+    }
+  }
 }
+
+module.exports = Board;
--- frontend/twodots.jsx
+++ frontend/twodots.jsx
@@ -1,7 +1,12 @@
 const spaget = () => console.log('spaghet');
-
+const Board = require('./board')
 
 document.addEventListener('DOMContentLoaded', ()=>{
   window.spaget = spaget;
-  init();
+  let board = new Board();
+  board.makeStage();
+  window.canvas = document.getElementById('Canvas');
+  window.makeCircle = board.makeCircle.bind(board);
+  window.stage=board.stage
+  // init();
 });
